ae4f8addf5cb1815864859b1662182772fa1a63b
oureveryday <oureveryday@163.com>
2025-04-20 00:07:01 +0800

    Update 2164030 manifest files

--- 2164030.lua
+++ 2164030.lua
@@ -1,5 +1,8 @@
 addappid(2164030)
+addappid(228989)
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
 addappid(2164031,0,"acc8f5c990c5cfa10b5d07a4d7fef8c3c1208311468bd51ddf170929cefc3cdb")
 setManifestid(2164031,"4481842819952198889")
+addappid(2164032)
+addappid(2164033)
