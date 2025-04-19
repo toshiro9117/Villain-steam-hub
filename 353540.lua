fec66810ef540c634ffff0190d5e73b78d82d024
oureveryday <oureveryday@163.com>
2025-04-20 00:02:36 +0800

    Update 353540 manifest files

--- 353540.lua
+++ 353540.lua
@@ -1,3 +1,6 @@
 addappid(353540)
+addappid(353541)
+addappid(353542)
+addappid(353543)
 addappid(353544,0,"3202fc49ee1db5ef5cbb7e67f7887b0af68493422f632915a4721ad414e0bea0")
 setManifestid(353544,"642317221497486381")
