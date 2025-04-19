e26f629825a76d0d8a10eac2fe225e2b90d3d996
oureveryday <oureveryday@163.com>
2025-04-19 23:37:44 +0800

    Update 700330 manifest files

--- 700330.lua
+++ 700330.lua
@@ -1,3 +1,7 @@
 addappid(700330)
+addappid(228989)
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
+addappid(229007)
+addappid(700331,0,"3d5c02fae29037cd244fb3cd9fc0c41a75f3a6c62bcb306fb39e79d0d5f6f2bd")
+addappid(700332)
