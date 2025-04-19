90b2c43aff7c5d673bbe427b69ef2c3c772c0205
oureveryday <oureveryday@163.com>
2025-04-19 23:52:08 +0800

    Update 1021210 manifest files

---
+++ 1021210.lua
@@ -0,0 +1,6 @@
+addappid(1021210)
+addappid(1021212,0,"ca8eecda387085ee0ddead4b8e6f11eb31acbf246bff8341d302488f02613fc4")
+setManifestid(1021212,"4439921680530252540")
+addappid(1021214)
+addappid(1021217)
+addappid(1021213)
